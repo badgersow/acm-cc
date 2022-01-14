c97be74c5e2b0a2c10a87dbab4ceddab89df0e64
Efim Pyshnograev <efim@fastmail.com>
2022-01-14 20:57:49 +1100

    Solve the same problem, but with the vector alone

--- Problem1383.cc
+++ Problem1383.cc
@@ -1,8 +1,4 @@
-#include <algorithm>
 #include <queue>
-#include <set>
-#include <unordered_map>
-#include <utility>
 #include <vector>
 
 namespace Problem1383 {
@@ -10,34 +6,34 @@ class Solution {
 public:
   int maxPerformance(int n, std::vector<int> &speed,
                      std::vector<int> &efficiency, int k) {
-    std::unordered_map<int, std::vector<int>> speeds_by_efficiency;
-    std::set<int> efficiencies;
+    std::vector<std::pair<int, int>> e_speed;
     for (int i = 0; i < speed.size(); ++i) {
-      efficiencies.insert(efficiency[i]);
-      speeds_by_efficiency[efficiency[i]].push_back(speed[i]);
+      e_speed.push_back(std::make_pair(efficiency[i], speed[i]));
     }
-    std::priority_queue<int, std::vector<int>, std::greater<int>>
-        hired; // HIRED
+    std::sort(e_speed.begin(), e_speed.end(),
+              [](std::pair<int, int> first, std::pair<int, int> second) {
+                return first.first >
+                       second.first; // larger efficiency comes first
+              });
+    std::priority_queue<int, std::vector<int>, std::greater<int>> hired;
     std::int64_t speed_sum = 0;
     std::int64_t best_result = 0;
     const std::int64_t P = 1000000007;
-    for (auto eff = efficiencies.rbegin(); eff != efficiencies.rend(); ++eff) {
-      int current_efficiency = *eff;
-      // Hire all the developers, if that makes any sense.
-      for (int speed : speeds_by_efficiency[current_efficiency]) {
-        if (hired.size() < k) {
-          // If we don't have the full team
-          hired.push(speed);
-          speed_sum += speed;
-        } else if (hired.top() < speed) {
-          // If the current slowest guy is worse, we need to replace it
-          speed_sum -= hired.top();
-          hired.pop();
-          hired.push(speed);
-          speed_sum += speed;
-        }
-        best_result = std::max(best_result, current_efficiency * speed_sum);
+    for (std::pair<int, int> pair : e_speed) {
+      int current_efficiency = pair.first;
+      int speed = pair.second;
+      if (hired.size() < k) {
+        // If we don't have the full team
+        hired.push(speed);
+        speed_sum += speed;
+      } else if (hired.top() < speed) {
+        // If the current slowest guy is worse, we need to replace it
+        speed_sum -= hired.top();
+        hired.pop();
+        hired.push(speed);
+        speed_sum += speed;
       }
+      best_result = std::max(best_result, current_efficiency * speed_sum);
     }
     return best_result % P;
   }
